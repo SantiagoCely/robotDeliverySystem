6d62450146d815ccdd5833c4b8f275fa7afeaf1a
SantiagoCely <55465565+SantiagoCely@users.noreply.github.com>
2021-11-21 20:47:49 -0500

    application

---
+++ application/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { environment } from '@env/environment';
+import { CoreModule } from '@core';
+import { SharedModule } from '@shared';
+import { HomeModule } from './home/home.module';
+import { ShellModule } from './shell/shell.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FallbackComponent } from './fallback.component';
+import { ShouldLoginComponent } from './should-login.component';
+
+import { NgHttpLoaderModule } from 'ng-http-loader';
+@NgModule({
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('./ngsw-worker.js', {
+      enabled: environment.production,
+    }),
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
+    NgHttpLoaderModule.forRoot(),
+    NgbModule,
+    CoreModule.forRoot(),
+    SharedModule,
+    ShellModule,
+    HomeModule,
+    AppRoutingModule, // must be imported as the last module as it contains the fallback route
+  ],
+  declarations: [AppComponent, FallbackComponent, ShouldLoginComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ application/src/app/credentials.ts
@@ -0,0 +1,11 @@
+// google firestore Credentials
+// https://ionicframework.com/blog/building-ionic-apps-with-firestore/
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBJUV6W_YqCx8hZ7Wo1Dvt1K3EDug1nB_w',
+  authDomain: 'ceg4912-3.firebaseapp.com',
+  projectId: 'ceg4912-3',
+  storageBucket: 'ceg4912-3.appspot.com',
+  messagingSenderId: '217552768659',
+  appId: '1:217552768659:web:54631bcefa32bdf76c0766',
+  measurementId: 'G-CS6XDTTXGF',
+};
