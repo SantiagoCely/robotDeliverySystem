b0cae91091b78b4284557f18e22bd6f3d2e40b5c
ata2151 <78291945+ata2151@users.noreply.github.com>
2021-11-23 22:35:37 -0500

    added_robot_to_environment and updated the environment

--- path_planner/environment_v1.py
+++ path_planner/environment_v1.py
@@ -6,7 +6,9 @@ import hardware_state
 import abc
 import tensorflow as tf
 import numpy as np
-
+import pybullet_data
+import pybullet as pb
+import time
 from tf_agents.environments import py_environment
 from tf_agents.environments import tf_environment
 from tf_agents.environments import tf_py_environment
@@ -16,14 +18,29 @@ from tf_agents.environments import wrappers
 from tf_agents.environments import suite_gym
 from tf_agents.trajectories import time_step as ts
 
+PATH_TO_TABLE = "/Users/amenabshir/PycharmProjects/capstone/robotDeliverySystem/path_planner/table/table.urdf"
+PATH_TO_TRAY = "/Users/amenabshir/PycharmProjects/capstone/robotDeliverySystem/path_planner/tray/tray.urdf"
+PATH_TO_ROBOT = "/Users/amenabshir/PycharmProjects/capstone/robotDeliverySystem/path_planner/robotv1.urdf"
+
 class PyEnvironment(object):
 
   def __init__(self):
-    self.env = image_space.ImageSpace()
+    pb.DIRECT
+    pb.setAdditionalSearchPath(pybullet_data.getDataPath())
+    pb.loadURDF('plane.urdf')
+    for x in range(-5, 5, 2):
+      for y in range(-5, 5, 2):
+        startPos = [y, x, 0]
+        startOrientation = pb.getQuaternionFromEuler([0, 0, 0])
+        pb.loadURDF(PATH_TO_TABLE,startPos, startOrientation)
+    startPos = [0, 1, 1]
+    pb.loadURDF(PATH_TO_TRAY, startPos,
+                startOrientation)
+    pb.setGravity(0, 0, -9.8)
+    startPos = [0, 0, 3]
+    pb.loadURDF(PATH_TO_ROBOT, startPos,
+                startOrientation)
     self.bot=hardware_state.Bot()
-    self.x=50
-    self.y=50
-
     if self.bot.get_battery_level()>20:
       self._state='high'
     else:
@@ -31,18 +48,23 @@ class PyEnvironment(object):
     self._episode_ended = False
 
   def _reset(self):
-    self._state = 0
+    if self.bot.get_battery_level() > 20:
+      self._state = 'high'
+    else:
+      self._state = 'low'
     self._episode_ended = False
     return ts.restart(np.array([self._state], dtype=np.int32))
+
   def reset(self):
     """Return initial_time_step."""
+
     self._current_time_step = self._reset()
     return self._current_time_step
 
   def step(self, action):
     """Apply action and return new time_step."""
 
-    if self.state=='staff_req' and action=='move_up':
+    #if self.state=='staff_req' and action=='move_up':
 
 
 
