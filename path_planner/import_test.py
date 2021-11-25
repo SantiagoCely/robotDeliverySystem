6ccca09423f15eff5b4446aad6da1a8d3a265491
ata2151 <78291945+ata2151@users.noreply.github.com>
2021-11-25 14:11:32 -0500

    Delete import_test.py

--- path_planner/import_test.py
+++
@@ -1,6 +0,0 @@
-import pybullet_data
-import pybullet as pb
-import time
-physicsClient = pb.connect(pb.GUI)
-pb.setAdditionalSearchPath(pybullet_data.getDataPath())
-pb.restoreState(fileName='/Users/amenabshir/PycharmProjects/capstone/robotDeliverySystem/path_planner/test.bullet')
