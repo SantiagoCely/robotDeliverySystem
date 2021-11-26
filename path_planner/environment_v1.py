cc7941728beb6ed2ff7f0232d6ef6b361444099d
ata2151 <78291945+ata2151@users.noreply.github.com>
2021-11-25 21:21:47 -0500

    env_update

--- path_planner/environment_v1.py
+++ path_planner/environment_v1.py
@@ -6,12 +6,12 @@ from abc import ABC
 
 import image_space
 import hardware_state
-import HighLevelController
+# import HighLevelController
 import abc
 import tensorflow as tf
 import numpy as np
 import pybullet_data
-import pybullet as pb
+import pybullet as p
 import time
 from tf_agents.environments import py_environment
 from tf_agents.environments import tf_environment
@@ -34,72 +34,94 @@ class CustomEnv(py_environment.PyEnvironment, ABC):
     REWARD_MOVE_DOWN = -1
     REWARD_MOVE_LEFT = -1
     REWARD_MOVE_RIGHT = -1
+    states = {
+        "high": 0,
+        "low": 1,
+        "staff_req": 2,
+        "cust_req": 3,
+        "host_req": 4,
+        "done": 5,
+        "collision": 6,
+
+    }
+
+    actions = {
+        "move": 0,
+        "move": 1,
+        "move": 2,
+        "move": 3,
+        "capture": 4,
+        "wait": 5,
+        "recharge": 6,
+    }
 
     def __init__(self):
-        pb.connect(pb.DIRECT)  # no GUI
-        pb.setAdditionalSearchPath(pybullet_data.getDataPath())
-        pb.loadURDF('plane.urdf')
+        self._p = p
+        self._p.connect(self._p.DIRECT)  # no GUI
+        self._p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        self._p.loadURDF('plane.urdf')
         for x in range(-5, 5, 2):  # generate table
             for y in range(-5, 5, 2):
                 startPos = [y, x, 0]
-                startOrientation = pb.getQuaternionFromEuler([0, 0, 0])
-                pb.loadURDF(PATH_TO_TABLE, startPos, startOrientation)
+                startOrientation = p.getQuaternionFromEuler([0, 0, 0])
+                self._p.loadURDF(PATH_TO_TABLE, startPos, startOrientation)
         startPos = [0, 1, 1]
-        pb.loadURDF(PATH_TO_TRAY, startPos,
-                    startOrientation)  # generate tray
-        pb.setGravity(0, 0, -9.8)
+        self._p.loadURDF(PATH_TO_TRAY, startPos,
+                         startOrientation)  # generate tray
+        self._p.setGravity(0, 0, -9.8)
         startPos = [0, 0, 3]
-        pb.loadURDF(PATH_TO_ROBOT, startPos,
-                    startOrientation)
-        self.projectionMatrix = pb.computeProjectionMatrixFOV(
-    fov=45.0,
-    aspect=1.0,
-    nearVal=0.1,
-    farVal=3.1)
+        self._p.loadURDF(PATH_TO_ROBOT, startPos,
+                         startOrientation)
+        self.projectionMatrix = self._p.computeProjectionMatrixFOV(
+            fov=45.0,
+            aspect=1.0,
+            nearVal=0.1,
+            farVal=3.1)
 
-        self.viewMatrix = pb.computeViewMatrix(
-    cameraEyePosition=[0, 0, 3],
-    cameraTargetPosition=[0, 0, 0],
-    cameraUpVector=[0, 1, 0])
+        self.viewMatrix = self._p.computeViewMatrix(
+            cameraEyePosition=[0, 0, 3],
+            cameraTargetPosition=[0, 0, 0],
+            cameraUpVector=[0, 1, 0])
+
         self.bot = hardware_state.Bot()
+        self._p.saveBullet("initial_state.bullet")
         hlc = HighLeveLController()
         if self.bot.get_battery_level() > 20:
-            self._state = 'high'
+            self._state = states["high"]
         else:
-            self._state = 'low'
+            self._state = states["low"]
         self._action_spec = array_spec.BoundedArraySpec(
-          shape=(), dtype=np.int32, minimum=0, maximum=6, name='action')
+            shape=(), dtype=np.int32, minimum=0, maximum=6, name='action')
         self._observation_spec = array_spec.BoundedArraySpec(
-          shape=(224,224), dtype=np.float, minimum=0, name='observation')
+            shape=(224, 224), dtype=np.float, minimum=0, name='observation')
         self.reward = -5
         self._episode_ended = False
-
-
+        self._envStepCounter = 0
 
     def observation_spec(self):
-      _, _, _, depthImg, _ = pb.getCameraImage(
-        width=224,
-        height=224,
-        viewMatrix=self.viewMatrix,
-        projectionMatrix=self.projectionMatrix)
-      self._observation_spec=depthImg
-      """Return observation_spec."""
+        _, _, _, depthImg, _ = pb.getCameraImage(
+            width=224,
+            height=224,
+            viewMatrix=self.viewMatrix,
+            projectionMatrix=self.projectionMatrix)
+        self._observation_spec = depthImg
+        """Return observation_spec."""
+        return self._observation_spec
 
     def action_spec(self):
-      return self._action_spec
-      """Return action_spec."""
+        return self._action_spec
+        """Return action_spec."""
 
     def _reset(self):
-      """
-  TO DO : - Reset robot postion
-          -
-  """
-      if self.bot.get_battery_level() > 20:
-          self._state = 'high'
-      else:
-          self._state = 'low'
-      self._episode_ended = False
-      return ts.restart(np.array([self._state], dtype=np.int32))
+
+        if self.bot.get_battery_level() > 20:
+            self._state = 1
+        else:
+            self._state = 2
+        self._episode_ended = False
+        self._p.restoreState(fileName="initial_state.bullet")
+        self._p.setTimeStep(self._time)
+        return ts.restart(np.array([self._state], dtype=np.int32))
 
     def _step(self, action):
         """Apply action and return new time_step."""
--- path_planner/hardware_state.py
+++ path_planner/hardware_state.py
@@ -1,9 +1,53 @@
+import time
+
+
 class Bot:
     def __init__(self):
         self.battery_level = 50
+        self.RIGHT_JOINT = 1
+        self.LEFT_JOINT = 2
+        self.MAX_FORCE = 500
+        self.TARGET_VEL = 10
 
     def get_battery_level(self):
         return self.battery_level
 
     def set_battery_level(self, val):
         self.battery_level = val
+
+    def move_left(self, p, robot_id):
+        p.sendJointMotorControl2(robot_id, self.RIGHT_JOINT, p.VELOCITY_CONTROL, targetVelocity=self.TARGET_VEL,
+                                 force=self.MAX_FORCE)
+        time.sleep(0.5)
+        p.sendJointMotorControl2(robot_id, self.RIGHT_JOINT, p.VELOCITY_CONTROL, targetVelocity=self.TARGET_VEL,
+                                 force=0)
+
+    def move_right(self, p, robot_id):
+        p.sendJointMotorControl2(robot_id, self.LEFT_JOINT, p.VELOCITY_CONTROL, targetVelocity=self.TARGET_VEL,
+                                 force=self.MAX_FORCE)
+        time.sleep(0.5)
+        p.sendJointMotorControl2(robot_id, self.LEFT_JOINT, p.VELOCITY_CONTROL, targetVelocity=self.TARGET_VEL,
+                                 force=self.MAX_FORCE)
+
+    def move_up(self, p, robot_id):
+        p.sendJointMotorControl2(robot_id, self.LEFT_JOINT, p.VELOCITY_CONTROL, targetVelocity=self.TARGET_VEL,
+                                 force=self.MAX_FORCE)
+        p.sendJointMotorControl2(robot_id, self.RIGHT_JOINT, p.VELOCITY_CONTROL, targetVelocity=self.TARGET_VEL,
+                                 force=self.MAX_FORCE)
+        time.sleep(0.5)
+        p.sendJointMotorControl2(robot_id, self.LEFT_JOINT, p.VELOCITY_CONTROL, targetVelocity=self.TARGET_VEL,
+                                 force=0)
+        p.sendJointMotorControl2(robot_id, self.RIGHT_JOINT, p.VELOCITY_CONTROL, targetVelocity=self.TARGET_VEL,
+                                 force=0)
+
+    def move_down(self, p, robot_id):
+        p.sendJointMotorControl2(robot_id, self.RIGHT_JOINT, p.VELOCITY_CONTROL, targetVelocity=self.TARGET_VEL,
+                                 force=self.MAX_FORCE)
+        time.sleep(0.25)
+        p.sendJointMotorControl2(robot_id, self.RIGHT_JOINT, p.VELOCITY_CONTROL, targetVelocity=self.TARGET_VEL,
+                                 force=0)
+        p.sendJointMotorControl2(robot_id, self.LEFT_JOINT, p.VELOCITY_CONTROL, targetVelocity=self.TARGET_VEL,
+                                 force=self.MAX_FORCE)
+        p.sendJointMotorControl2(robot_id, self.RIGHT_JOINT, p.VELOCITY_CONTROL, targetVelocity=self.TARGET_VEL,
+                                 force=self.MAX_FORCE)
+        time.sleep(0.5)
